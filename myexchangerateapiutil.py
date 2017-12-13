e4cb8483ac538d91a82de8aa84fac3c5046d70dd
anirC <anirban.chakraborty@student.fairfield.edu>
2017-12-12 19:06:06 -0500

    latest

--- myexchangerateapiutil.py
+++ myexchangerateapiutil.py
@@ -8,7 +8,7 @@ import urllib.parse
 from pandas.io.json import json_normalize
 
 
-def import_ExchangeRate(toCurr,onDate):
+def get_Exchangerate(toCurr,histDate):
     """Uses the openexchangerates API to look up FX rate information
     base -- the base currency (always USD)
     toCurr -- the currencies to be converted into USD(optional)
@@ -16,19 +16,20 @@ def import_ExchangeRate(toCurr,onDate):
 
     #set app code
     app_code = '09e66a35af4b4736b8ec7111ed699942'
-    url= 'https://openexchangerates.org/api/latest.json?app_id='+ app_code + urllib.parse.urlencode({'&symbols':toCurr})
+    url= 'https://openexchangerates.org/api/historical/'+ histDate + '.json?app_id='+ app_code + '&symbols=' + toCurr
+
     try:
-        fx_rate = requests.get(url).json()
+        fx = requests.get(url).json()
     except:
         # failed api lookup
         raise ValueError("No exchange rate for the given currency or date could be found")
-    rate = pd.DataFrame(fx_rate['rates'])
+
 
     return {'rates':rate}
 
-
 def get_Currencies():
-
+    """Uses the openexchangerates API to look up currency code, name combination
+    """
 
     #set app code
     app_code = '09e66a35af4b4736b8ec7111ed699942'
